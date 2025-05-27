0f1ade26178cd5595e22c121de217e175dff250c
Mightyraider <kristian.kolev@hotmail.com>
2025-05-27 19:24:42 +0200

    Updated default pawn

--- Source/Plugin_Development/Plugin_DevelopmentGameMode.cpp
+++ Source/Plugin_Development/Plugin_DevelopmentGameMode.cpp
@@ -7,9 +7,10 @@
 APlugin_DevelopmentGameMode::APlugin_DevelopmentGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game//Heroes/BP_HeroCharacter_Test")); //F:/UnrealProjects/Plugin_Development/Content/Heroes/BP_HeroCharacter_Test.uasset /Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+		
 	}
 }
