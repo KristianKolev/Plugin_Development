3e8fbd479b08dfcf5cd72f19af333b38250abe36
Mightyraider <kristian.kolev@hotmail.com>
2025-05-28 21:25:40 +0200

    Added Enums for category, aspect and resourcetype. Added struct with mesh,material and niagara system that can be set on the component inside BPs

--- Source/Plugin_Development/Upgradable_Assets/UpgradeLevelData.h
+++ Source/Plugin_Development/Upgradable_Assets/UpgradeLevelData.h
@@ -1,11 +1,10 @@
-
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "NiagaraSystem.h"
 #include "UpgradeLevelData.generated.h"
 
-USTRUCT(BlueprintType)
+USTRUCT(BlueprintType)	//Maybe unnecessary and replaced by enum
 struct PLUGIN_DEVELOPMENT_API FResourceType
 {
 	GENERATED_BODY()
@@ -17,6 +16,20 @@ struct PLUGIN_DEVELOPMENT_API FResourceType
 	FString Name {"Stuff"};
 };
 
+UENUM(BlueprintType)
+enum class EResourceType : uint8
+{
+	None = 0,
+	Food,
+	Water,
+	Wood,
+	Stone,
+	Iron,
+	Gold,
+	Moonstone,
+	Energy,
+	Power
+};
 
 USTRUCT(BlueprintType)
 struct PLUGIN_DEVELOPMENT_API FUpgradeLevelData
@@ -24,8 +37,45 @@ struct PLUGIN_DEVELOPMENT_API FUpgradeLevelData
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TArray<FResourceType> ResourceTypes;
+	TArray<EResourceType> ResourceTypes;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TArray<int32> UpgradeCosts;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<int32> UpgradeSeconds;
+};
+
+USTRUCT(BlueprintType)
+struct PLUGIN_DEVELOPMENT_API FUpgradeLevelDataVisuals
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<int32, UStaticMesh*> StaticMeshPerLevel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<int32, UMaterialInstance*> MaterialInstancePerLevel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<int32, UNiagaraSystem*> NiagaraSystemPerLevel;
+};
+
+UENUM(BlueprintType)
+enum class EUpgradableCategory : uint8
+{
+	None = 0,
+	Unit,
+	Building,
+	Equipment
+};
+
+UENUM(BlueprintType)
+enum class EUpgradableAspect : uint8
+{
+	None = 0,
+	Level,
+	Tier,
+	Rank,
+	Star
 };
