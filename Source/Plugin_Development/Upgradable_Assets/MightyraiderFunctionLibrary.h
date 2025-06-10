ade8f69d0cabf25756cc3c43f76b17d7794de4fe
Mightyraider <kristian.kolev@hotmail.com>
2025-06-10 10:57:28 +0200

    Created a custom function library to provide useful functions

---
+++ Source/Plugin_Development/Upgradable_Assets/MightyraiderFunctionLibrary.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MightyraiderFunctionLibrary.generated.h"
+
+/**
+ * A collection of functions that I find useful and need to use in many projects.
+ */
+UCLASS()
+class PLUGIN_DEVELOPMENT_API UMightyraiderFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	/* Display the Project version number */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Game Config")
+	static FString GetProjectVersion();
+
+	
+	
+};
