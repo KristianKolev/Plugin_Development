59f9166925c416e62218241d5dec6405b1065b3b
Mightyraider <kristian.kolev@hotmail.com>
2025-07-16 10:28:18 +0200

    Defined a custom logging header to be used for the logging categories of each system.

---
+++ Source/Plugin_Development/CustomLogging.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Logging/StructuredLog.h"
+
+/* Custom logging category for the upgrade system */
+DECLARE_LOG_CATEGORY_EXTERN(UpgradeSystemLog, Log, All)
+
+/* Custom logging category for the resource system */
+DECLARE_LOG_CATEGORY_EXTERN(ResourceSystemLog, Log, All)
--- Source/Plugin_Development/Plugin_Development.cpp
+++ Source/Plugin_Development/Plugin_Development.cpp
@@ -1,7 +1,11 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Plugin_Development.h"
+#include "CustomLogging.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Plugin_Development, "Plugin_Development" );
+
+DEFINE_LOG_CATEGORY(UpgradeSystemLog);
+DEFINE_LOG_CATEGORY(ResourceSystemLog);
  
